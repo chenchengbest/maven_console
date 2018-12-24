6c074a0387d153c4f14340464b20fd6bb94cc079
cc0027010605 <wb-cc403831@alibaba-inc.com>
2018-12-24 09:55:52 +0800

    add:学习计划

--- src/main/java/com/hustlion/java_console_spring/App.java
+++ src/main/java/com/hustlion/java_console_spring/App.java
@@ -2,21 +2,23 @@ package com.hustlion.java_console_spring;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import zookeeper_lock.ZookeeperLockTest;
 
 /**
  * Hello world!
  *
  */
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// 获取 classpath 中的 Beans.xml 到上下文 context
 		// 此处即 /java_console_spring/src/main/java/Beans.xml
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-		// 获取在 Beans.xml 中定义的 id 为 helloWorld 的 bean
-		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-		// 使用获得的 bean 的 getMessage 方法
-		obj.getMessage();
-		// 关闭上下文，防止内存泄漏
-		((ClassPathXmlApplicationContext) context).close();
+//		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+//		// 获取在 Beans.xml 中定义的 id 为 helloWorld 的 bean
+//		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+//		// 使用获得的 bean 的 getMessage 方法
+//		obj.getMessage();
+//		// 关闭上下文，防止内存泄漏
+//		((ClassPathXmlApplicationContext) context).close();
+		ZookeeperLockTest.test();
 	}
 }
---
+++ src/main/java/zookeeper_lock/ZookeeperLockTest.java
@@ -0,0 +1,36 @@
+package zookeeper_lock;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+/**
+ * The type Zookeeper lock test.
+ */
+public class ZookeeperLockTest {
+    /**
+     * Test.
+     *
+     * @throws Exception the exception
+     */
+    public static void test() throws Exception {
+        //创建zookeeper的客户端
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("192.168.1.107:2181", retryPolicy);
+        client.start();
+
+        //创建分布式锁, 锁空间的根节点路径为/curator/lock
+        InterProcessMutex mutex = new InterProcessMutex(client, "/curator/lock");
+        mutex.acquire();
+        //获得了锁, 进行业务流程
+        System.out.println("Enter mutex");
+        //完成业务流程, 释放锁
+        mutex.release();
+
+        //关闭客户端
+        client.close();
+    }
+
+}
